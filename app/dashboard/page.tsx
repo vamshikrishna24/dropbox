ffd389875b3f411b30f876c4fe55d2a5bad4b4d9
vamshikrishna24 <vamshikrishna2124@gmail.com>
2024-01-26 18:36:29 +0530

    Skeleton loader

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,36 @@
 import Dropzone from "@/components/Dropzone";
+import { db } from "@/firebase";
 import { auth } from "@clerk/nextjs";
+import { collection, getDocs } from "firebase/firestore";
 import React from "react";
+import { FileType } from "@/typings";
+import TableWrapper from "@/components/table/TableWrapper";
 
-function Dashboard() {
+async function Dashboard() {
   const { userId } = auth();
+
+  const docResults = await getDocs(collection(db, "users", userId!, "files"));
+  const skeletonFiles: FileType[] = docResults.docs.map((doc) => ({
+    id: doc.id,
+    filename: doc.data().filename || doc.id,
+    timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    fullName: doc.data().fullName,
+    downloadUrl: doc.data().downloadUrl,
+    type: doc.data().type,
+    size: doc.data().size,
+  }));
+
+  // console.log(skeletonFiles);
   return (
-    <div>
+    <div className="border-t">
       <Dropzone />
+
+      <section className="container space-y-5">
+        <h2 className="font-bold">All Files</h2>
+        <div>
+          <TableWrapper skeletonFiles={skeletonFiles} />
+        </div>
+      </section>
     </div>
   );
 }
---
+++ constants.ts
@@ -0,0 +1,7 @@
+export const COLOR_EXTENSION_MAP: { [key: string]: string } = {
+  pdf: "#0160FE",
+  jpg: "#d4af337",
+  jpeg: "#d4af337",
+  doc: "#2b579a",
+  docx: "#2b579a",
+};
