5d3da3bf18fd4a444cc06982651ff9dba76ec5dc
vamshikrishna24 <vamshikrishna2124@gmail.com>
2024-01-26 21:13:33 +0530

    env declaration

--- firebase.ts
+++ firebase.ts
@@ -7,12 +7,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8VDBvvIyZYMZC6JzWS1GGm-1F2XDazFc",
-  authDomain: "dropbox-clone-22a00.firebaseapp.com",
-  projectId: "dropbox-clone-22a00",
-  storageBucket: "dropbox-clone-22a00.appspot.com",
-  messagingSenderId: "218435169759",
-  appId: "1:218435169759:web:31ecabc475e5f98678a86a",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
